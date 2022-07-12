cf1e4d0877689a9b19f5f317f3b8cf40519a6462
GitHub cksthf3211 <GitHob cksthf321@gmail.com>
2022-07-12 23:39:58 +0900

    수업 정리노트

--- 수업 정리노트/2022.07.11-codeup.py
+++ 수업 정리노트/2022.07.11-codeup.py
@@ -1,2 +1,12 @@
-num = input(15)
-print(int, type(num))
+dust = 100
+# dust가 150보다 크면, 매우 나쁨
+if dust > 150:
+    print('매우나쁨')
+# dust가 80보다 크면, 나쁨
+elif dust > 80:
+    print('나쁨')
+# dust가 30보다 크면, 보통
+elif dust > 30:
+    print('보통')
+else:
+    print('좋음')
