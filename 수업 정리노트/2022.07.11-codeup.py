4287fdad38d259e8bd11c5191b2ddbfe4368c42a
GitHub cksthf3211 <cksthf321@gmail.com>
2022-07-14 18:00:22 +0900

    수업 정리노트

--- 수업 정리노트/2022.07.11-codeup.py
+++ 수업 정리노트/2022.07.11-codeup.py
@@ -1,5 +1,11 @@
-n = int(input())
-if n % 3 == 0 and n % 2 == 0:
-    print("참")
-else:
-    print("거짓")
+word = 'banana'
+result = {}
+for char in word:
+    # 딕셔너리에 키가 있으면
+    if char in result:
+        # 키랑 값을 0으로 초기화
+        result[char] = 1
+    # 딕셔너리에 키가 없으면
+    else:
+        result[char] = result[char] + 1
+print(result)        
