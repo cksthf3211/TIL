db36f63581870cb86110699168dfd1590a3f1f1a
GitHub cksthf3211 <cksthf321@gmail.com>
2022-07-15 18:50:49 +0900

    수업 정리노트

--- 수업 정리노트/2022.07.11-codeup.py
+++ 수업 정리노트/2022.07.11-codeup.py
@@ -1,11 +1,2 @@
-word = 'banana'
-result = {}
-for char in word:
-    # 딕셔너리에 키가 있으면
-    if char in result:
-        # 키랑 값을 0으로 초기화
-        result[char] = 1
-    # 딕셔너리에 키가 없으면
-    else:
-        result[char] = result[char] + 1
-print(result)        
+a, b = input().split('-')
+print(a + b)
