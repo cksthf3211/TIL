16c5731fc89147bf9c88f538e922d2819e62f63c
GitHub cksthf3211 <cksthf321@gmail.com>
2022-07-20 17:37:57 +0900

    정리노트

--- 수업 정리노트/2022.py
+++ 수업 정리노트/2022.py
@@ -1,10 +1,9 @@
 import sys
+
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
-for i in range(1, T + 1):
-
-    a = map(int, input().split())
-    
-    print(f"a: {a} T: {T}")
+for i in range(1, T+1):
+    a, b = map(int, input().split())
+    print("#{} {} {}".format(i, a//b, a%b))
