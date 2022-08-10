68f96ca0dc09b67d7b96e8ba8681b60a398be015
cksthf3211 <cksthf321@gmail.com>
2022-08-10 23:17:17 +0900

    수업 정리노트

--- python1.py
+++ python1.py
@@ -1,20 +1,13 @@
-basedata = '''김영민, 69, 100, 100
-박동식, 100, 98, 98
-최민규, 88, 96, 90
-동후민, 88, 98, 97
-신형균, 99, 44, 66
-김철규, 88, 69, 100'''
-data = basedata.split( '\n')
-
-for i in range(len(data)):
-    data[i] = data[i].split(',')
-    print(data[i])
-for k in range(len(data)):
-    sum = 0
-    for j in range(1, 4):
-        data[k][j] = int(data[k][j])
-        sum += data[k][j]
-    data[k].append(sum)
-    av = int(sum/3)
-    data[k].append(av)
-    print(data[k])
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x - 1)
+while True:
+    n = input('\n팩토리얼 구할 숫자는??')
+    if n.isnumeric():
+        res = factorial(int(n))
+        print(res)
+    else:
+        print('종료합니다.')
+        break
