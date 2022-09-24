208e233a12273652020da7ab3e7e044e58bed8c0
cksthf3211 <cksthf321@gmail.com>
2022-09-24 16:40:07 +0900

    수업 정리노트

--- py.py
+++ py.py
@@ -1,14 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-a = int(input())
-b = int(input())
-c = int(input())
-if a == b == c:
-    print('Equilateral')
-if a + b + c == 180 and a == b or a == c or b == c:
-    print('Isosceles')
-if a + b + c == 180 and a != b or a != c or b != c:
-    print('Scalene')
-else:
-    print('Error')
+n = int(input())
+cnt = 0
+
+for i in range(n):
+    num = list(map(int, input()))
+    avg = sum(num) / n
+    center = num.sort() # 중앙값 미완
+    bin = set(num) # 최빈값 미완
+    rang = len(max(num) - min(num))
+
