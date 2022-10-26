9f8931daac93b87dce07e04a581771a9a26ee8df
cksthf3211 <cksthf321@gmail.com>
2022-10-26 18:32:12 +0900

    비동기

--- Django/django12/articles/views.py
+++ Django/django12/articles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 from django.contrib import messages
@@ -60,7 +61,7 @@ def delete(request, pk):
     return redirect('articles:index')
 
 def comment(request, pk):
-    article = Article.objects.get(pk=pk)
+    article =get_object_or_404(Article, pk=pk)
     form = CommentForm(request.POST)
     if form.is_valid():
         comment = form.save(commit=False)
@@ -68,7 +69,11 @@ def comment(request, pk):
         comment.user = request.user
         messages.success(request, '댓글을 달았다.')
         comment.save()
-    return redirect('articles:detail', article.pk)
+        context = {
+            'content': comment.content,
+            'userName': comment.user.username
+        }
+    return JsonResponse(context)
 
 def comments_delete(request, article_pk, comment_pk):
     comment = Comment.objects.get(pk=comment_pk)
@@ -100,14 +105,17 @@ def comments_update (request,article_pk, comment_pk):
 
 @ login_required
 def like(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     # 만약에 로그인한 유저가 이 글을 좋아요를 눌렀다면,
     # if article.like_users.filter(id=request.user.id).exists():
     if request.user in article.like_users.all(): 
         # 좋아요 삭제하고
         article.like_users.remove(request.user)
+        is_liked = False
     else:
         # 좋아요 추가하고 
         article.like_users.add(request.user)
+        is_liked = True
     # 상세 페이지로 redirect
-    return redirect('articles:detail', pk)
+    context = {'isLiked': is_liked, 'likeCount': article.like_users.count()}
+    return JsonResponse(context)
