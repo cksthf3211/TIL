15fd7dbf8c54accea6159c204c56e9a91a1388aa
cksthf3211 <cksthf321@gmail.com>
2022-11-25 17:49:29 +0900

    수업 정리

--- py.py
+++ py.py
@@ -1,5 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
+x = 64
 n = int(input())
+cnt = 0
+
+while n > 0:
+    if x > n:
+        x = x // 2
+    else:
+        cnt += 1
+        n -= x
+print(cnt)
+    
