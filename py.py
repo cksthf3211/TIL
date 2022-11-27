0671ba0f4ecdf01a8c1ea450a6ad7c89f8d0faa2
cksthf3211 <cksthf321@gmail.com>
2022-11-27 16:21:02 +0900

    정리

--- py.py
+++ py.py
@@ -1,15 +1,4 @@
 import sys
 input = sys.stdin.readline
 
-x = 64
 n = int(input())
-cnt = 0
-
-while n > 0:
-    if x > n:
-        x = x // 2
-    else:
-        cnt += 1
-        n -= x
-print(cnt)
-    
