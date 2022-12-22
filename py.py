80ca57e71d5876e62556b02eb6f82c59af2a2b05
cksthf3211 <cksthf321@gmail.com>
2022-12-23 00:54:47 +0900

    qeue

--- py.py
+++ py.py
@@ -1,18 +1,16 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
  
-n = int(input())  # 색종이 수
+n = int(input())
+card = deque()
+
+for i in range(n, 0, -1):
+    card.append(i)                  # deque([6, 5, 4, 3, 2, 1])
+ 
+while len(card) > 1:
+    card.pop()                  # 1번 카드 버림 (1)
+    card.appendleft(card.pop()) # 2번 카드 맨 밑으로 
+    
+print(card[0])
 paper = [[0 for i in range(101)] for i in range(101)] # 2차원 배열 생성
-
-for i in range(n):
-    x,y = map(int, input().split()) # input 받아옴
-    for j in range(x, x+10):        # x~ x+10까지 지정
-        for k in range(y, y+10):    # y~ y+10까지 지정
-            paper[j][k] = 1         # 다 1로 바꿔버림
-            
-cnt = 0
-
-for row in paper:
-    cnt += row.count(1)             # 2차원 배열인 paper 안에서 1로 바꾼 부분을 출력
-
-print(cnt)
