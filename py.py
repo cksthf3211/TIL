ee2b081054f555304e15bf2ee76acd44b56ef52b
cksthf3211 <cksthf321@gmail.com>
2022-12-26 14:09:02 +0900

    수정

--- py.py
+++ py.py
@@ -2,17 +2,3 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-# n = int(input())
-# n_list = []
-# num = deque()
-
-# for i in range(n):
-    
-#     while num:
-        
-#         print(-1)
-#         break
-for i in range(5):
-    for j in range(i+1):
-        print('*', end='')
-    print('')
