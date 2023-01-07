dd63dc5de107d812b7863900c99aefdaa041848c
cksthf3211 <cksthf321@gmail.com>
2023-01-07 14:29:03 +0900

    디자인패턴, 브라우저 흐름, 인증/인가, 컴파일러

--- py.py
+++ py.py
@@ -1,10 +1,5 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
-import math
 
-t = int(input())
+n = int(input())
 
-for _ in range(t):
-    n, m = map(int, input().split(' '))
-    print(math.comb(m, n))
