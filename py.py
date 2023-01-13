71d06cd7db4c050a4bbc1b810e1be2230f190d2b
cksthf3211 <cksthf321@gmail.com>
2023-01-13 18:58:03 +0900

    정리

--- py.py
+++ py.py
@@ -1,21 +1,12 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
+import heapq
 
-n = input()
-n_list_1 = list(map(int, input().split()))
-# print(n_list) [2, 4, -10, 4, -9]
+n = int(input())
+heap = []
 
-n_list_2 = sorted(list(set(n_list_1)))
-# print(n_list) [-10, -9, 2, 4]
+for i in range(n):
+    num = int(input())
 
-dic = {n_list_2[i] : i for i in range(len(n_list_2))}
-# print(dic) {-10: 0, -9: 1, 2: 2, 4: 3}
-# 아래와 같음
-# dic_1 = {}
-# for i in range(len(n_list_2)):
-#     dic_1[n_list_2[i]] = i
-# print(dic_1) {-10: 0, -9: 1, 2: 2, 4: 3}
-
-for i in n_list_1:
-    print(dic[i], end=' ') # 2 3 0 3 1
+print(num)
