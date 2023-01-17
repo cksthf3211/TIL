8434f93abc7b0d23eb4e41b4bee9f1b20737941d
cksthf3211 <cksthf321@gmail.com>
2023-01-18 01:28:52 +0900

    heapq

--- py.py
+++ py.py
@@ -4,10 +4,25 @@ from collections import deque
 import heapq
 
 n = int(input())
-heap = []
+n_list = []
+cnt = 0
 
+ 
 for i in range(n):
-    num = int(input())
-
-# print(num)
-# print(heap)
+    n_list.append(int(input()))
+if n == 1:
+    print(0)
+else:
+    dasom = n_list[0]
+    not_dasom = sorted(n_list[1:], reverse=True)
+    for index, num in enumerate(not_dasom):
+        if dasom == num:
+            print(1)
+            break
+        while(not_dasom[0] >= dasom):
+            cnt += 1
+            dasom += 1
+            not_dasom[0] -= 1
+            not_dasom = sorted(not_dasom, reverse=True)
+        print(cnt)
+        break
