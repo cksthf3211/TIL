ea9a03aeb1799c3a9b3cbdfb0568955a40f17569
cksthf3211 <cksthf321@gmail.com>
2023-01-25 15:35:01 +0900

    cs

--- py.py
+++ py.py
@@ -1,39 +1,39 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-import heapq
 sys.setrecursionlimit(10 ** 6)         #  재귀 깊이 늘려줌줌이
 
 n, m, r = map(int, input().split())    # 정점 수, 간선 수, 시작 정점
-graph = [[] for _ in range(n+1)]       # 연결노드 그래프 초기화 (1번노드와 인덱스 값이 같게 하기 위해서 n+1로 )
-cnt = 1                                # 방문 순서
-visited = [0] * (n+1)                  # 방문 순서 그래프
-
-# print(graph) [[], [], [], [], []]
+cnt = 1
+visited = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
 # print(visited) [0, 0, 0, 0, 0, 0]
+# print(graph) [[], [], [], [], [], []]
 
 for i in range(m):
     u, v = map(int, input().split())
-    graph[u].append(v)                 # u가 시작 정점, 연결 되있음 v랑
-    graph[v].append(u)                 # v가 끝나는 정점, 연결 되있음 u랑
-# graph.sort()
-# print(graph) [[], [4, 2], [1, 3, 4], [2, 4], [1, 2, 3], []]
+    graph[u].append(v)
+    graph[v].append(u)
 
-for i in graph:
-    i.sort(reverse=True)                           # 오름차순으로 정렬
-# print(graph) [[], [2, 4], [1, 3, 4], [2, 4], [1, 2, 3], []]
-print(graph)
-def dfs(graph, r, visited):            # v: 정점 집합, e: 간선 집합, r: 시작 정점
+def bfs(b):
     global cnt
-    visited[r] = cnt
-    for i in graph[r]:
-        if visited[i] == 0:
-            cnt += 1
-            dfs(graph, i, visited)
+    
+    q = deque([r])
+    visited[r] = 1
+    
+    while q:
+        b = q.popleft()
+        graph[b].sort()
+        
+        for i in graph[b]:
+            if visited[i] == 0:
+                cnt += 1
+                visited[i] = cnt
+                q.append(i)
+    
+bfs(1)
 
-dfs(graph, r, visited)
-# print(visited) [0, 1, 2, 3, 4, 0]
-# print(cnt) 4 
-
-for i in range(1, n+1):
-    print(visited[i])
+#print(visited) [0, 1, 2, 4, 3, 0]
+for i in visited[1:]:
+    print(i)
+    
