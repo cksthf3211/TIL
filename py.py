e31357452527e8b7512c9c6df4c953a3e3a3f24f
cksthf3211 <cksthf321@gmail.com>
2023-02-02 02:11:12 +0900

    cs

--- py.py
+++ py.py
@@ -1,10 +1,14 @@
 import sys
+sys.stdin = open('py.txt')
 input = sys.stdin.readline
 from collections import deque
+from pprint import pprint
 
 n = int(input())           # 지도의 크기
-cnt = 0
 graph = [list(map(int, input().rstrip())) for i in range(n)]  # 행렬 만들기
+n_list = []
+visited = [ [0] * n for i in range(n)]
+
 # print(graph)
 # [[0, 1, 1, 0, 1, 0, 0],
 #  [0, 1, 1, 0, 1, 0, 1],
@@ -15,13 +19,52 @@ graph = [list(map(int, input().rstrip())) for i in range(n)]  # 행렬 만들기
 #  [0, 1, 1, 1, 0, 0, 0]]
 
 # 한 점을 기준으로 (위 아래 왼쪽 오른쪽) 으로 한칸 씩 이동할 좌표
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+dy = [0,0,1,-1] # 위
+dx = [1,-1,0,0] # 아래
 
-def bfs(graph, x, y):
-    global cnt
+def bfs(y, x):
+    cnt = 1
+    queue = deque()
+    queue.append((y, x))
+    visited[y][x] = 1
     
-    if x < 0 or x >= n or y < 0 or y >= n:
-        cnt += 1
-    
-    
+    while queue:
+        y, x = queue.pop()
+                
+        if graph[y][x] == 1:
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < n and 0 <= ny < n and graph[ny][nx] == 1 and visited[ny][nx] == 0:
+                    queue.append((ny, nx))
+                    cnt += 1
+                    visited[ny][nx] = 1
+    return cnt
+
+for i in range(n):
+    for j in range(n):
+        
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            n_list.append(bfs(i, j))
+
+# pprint(visited)
+# [[0, 1, 1, 0, 1, 0, 0],
+#  [0, 1, 1, 0, 1, 0, 1],
+#  [1, 1, 1, 0, 1, 0, 1],
+#  [0, 0, 0, 0, 1, 1, 1],
+#  [0, 1, 0, 0, 0, 0, 0],
+#  [0, 1, 1, 1, 1, 1, 0],
+#  [0, 1, 1, 1, 0, 0, 0]]
+
+
+# pprint(n_list)
+# [7, 8, 9]
+
+n_list.sort() # 오름차순
+
+print(len(n_list)) # 총 단지 수
+
+for k in n_list:   # 각 단지마다 집의 수
+    print(k)
