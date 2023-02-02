5eeda8e1122adbc14d1b88e49e1b284e4504e0fd
cksthf3211 <cksthf321@gmail.com>
2023-02-02 20:12:24 +0900

    cs

--- py.py
+++ py.py
@@ -1,70 +1,41 @@
 import sys
-sys.stdin = open('py.txt')
+# sys.stdin = open('py.txt')
 input = sys.stdin.readline
 from collections import deque
 from pprint import pprint
 
-n = int(input())           # 지도의 크기
+n, m = map(int, input().split())           # 지도의 크기
 graph = [list(map(int, input().rstrip())) for i in range(n)]  # 행렬 만들기
 n_list = []
-visited = [ [0] * n for i in range(n)]
+visited = [ [0] * m for i in range(n)]
+cnt = 1
+
+# (1, 1)에서 출발 (N, M)의 위치로 이동
+# 지나야 하는 최소 칸 수 출력
 
 # print(graph)
-# [[0, 1, 1, 0, 1, 0, 0],
-#  [0, 1, 1, 0, 1, 0, 1],
-#  [1, 1, 1, 0, 1, 0, 1],
-#  [0, 0, 0, 0, 1, 1, 1],
-#  [0, 1, 0, 0, 0, 0, 0],
-#  [0, 1, 1, 1, 1, 1, 0],
-#  [0, 1, 1, 1, 0, 0, 0]]
+# [[1, 0, 1, 1, 1, 1], 
+# [1, 0, 1, 0, 1, 0], 
+# [1, 0, 1, 0, 1, 1], 
+# [1, 1, 1, 0, 1, 1]]
 
-# 한 점을 기준으로 (위 아래 왼쪽 오른쪽) 으로 한칸 씩 이동할 좌표
-dy = [0,0,1,-1] # 위
-dx = [1,-1,0,0] # 아래
+# print(visited)
+# [[0, 0, 0, 0, 0, 0], 
+# [0, 0, 0, 0, 0, 0], 
+# [0, 0, 0, 0, 0, 0], 
+# [0, 0, 0, 0, 0, 0]]
 
-def bfs(y, x):
-    cnt = 1
+dx = [0, 1, -1, 0]
+dy = [-1, 0, 0, 1]
+
+def bfs(x, y):
     queue = deque()
-    queue.append((y, x))
-    visited[y][x] = 1
+    queue.append(x, y)
     
     while queue:
-        y, x = queue.pop()
-                
-        if graph[y][x] == 1:
-            
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-
-                if 0 <= nx < n and 0 <= ny < n and graph[ny][nx] == 1 and visited[ny][nx] == 0:
-                    queue.append((ny, nx))
-                    cnt += 1
-                    visited[ny][nx] = 1
-    return cnt
-
-for i in range(n):
-    for j in range(n):
+        x, y = queue.popleft()
         
-        if graph[i][j] == 1 and visited[i][j] == 0:
-            n_list.append(bfs(i, j))
-
-# pprint(visited)
-# [[0, 1, 1, 0, 1, 0, 0],
-#  [0, 1, 1, 0, 1, 0, 1],
-#  [1, 1, 1, 0, 1, 0, 1],
-#  [0, 0, 0, 0, 1, 1, 1],
-#  [0, 1, 0, 0, 0, 0, 0],
-#  [0, 1, 1, 1, 1, 1, 0],
-#  [0, 1, 1, 1, 0, 0, 0]]
-
-
-# pprint(n_list)
-# [7, 8, 9]
-
-n_list.sort() # 오름차순
-
-print(len(n_list)) # 총 단지 수
-
-for k in n_list:   # 각 단지마다 집의 수
-    print(k)
+        # 4가지 방향으로 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
