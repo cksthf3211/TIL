f3a183f92b060ae3d529838b6fab5342ac5ff376
cksthf3211 <cksthf321@gmail.com>
2023-02-25 12:46:43 +0900

    수정

--- py.py
+++ py.py
@@ -33,15 +33,17 @@ def bfs(x, y):
     
     while queue:
         x, y = queue.popleft()
+        if x == n - 1 and y  == m - 1:
+            print(graph[x][y])
         
         # 4가지 방향으로 위치 확인
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0 < nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
                 queue.append((nx, ny))
-                graph[nx][ny] == graph[x][y] + 1
+                graph[nx][ny] = graph[x][y] + 1
             
             # if nx < 0 or ny < 0 or nx >= n or ny >= m: # 가장자리 벽
             #     continue
@@ -52,7 +54,4 @@ def bfs(x, y):
             # if graph[nx][ny] == 1:
             #     queue.append((nx, ny))
             #     graph[nx][ny] == graph[x][y] + 1
-                
-    return graph[n-1][m-1]
-
-print(bfs(0,0)) 
+bfs(0,0) 
